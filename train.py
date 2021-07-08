9c18a9cdb4e9692e7ea2a5ac03a0668c7f157403
Solano <solano@visitorhealth.com.br>
2021-07-08 17:27:15 -0300

    final changes to run

--- train.py
+++ train.py
@@ -3,6 +3,11 @@ import numpy as np
 import os
 import csv
 import pandas as pd
+import argparse
+import sys
+
+from tqdm import tqdm
+from parser_script import PathType
 from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
@@ -11,56 +16,46 @@ def input_fn(args):
 
   pos_class = os.listdir(args.pos_folder)
   neg_class = os.listdir(args.neg_folder)
-  pos_dfs = []
-  neg_dfs = []
-
-  for filename in pos_class:
-    file_path = os.path.join(args.pos_folder, filename)
-    data = pd.read_csv(file_path, delimiter=';', iterator=True, chunksize=1000)
-    data.drop(index=df.index[0], 
-              axis=0, 
-              inplace=True)
-    pos_dfs.append(data)
-
-  for filename in neg_class:
-    file_path = os.path.join(args.neg_folder, filename)
-    data = pd.read_csv(file_path, delimiter=';', iterator=True, chunksize=1000)
-    data.drop(index=df.index[0], 
-              axis=0, 
-              inplace=True)
-    neg_dfs.append(data)
-
-  return pos_dfs, neg_dfs
-
-def preprocess(pos_dfs, neg_dfs):
-
+  
   X = []
   Y = []
 
-  for df in pos_dfs:
-    arr = df.to_numpy()
+  for filename in pos_class:
+    file_path = os.path.join(args.pos_folder, filename)
+    data = pd.read_csv(file_path, sep=',', header = None)
+    data.drop(index=data.index[0], 
+              axis=0, 
+              inplace=True)
+    data = data.apply(pd.to_numeric)
+    arr = data.to_numpy()
+    np.reshape(arr,(1, 18001, 15))
     X.append(arr)
     Y.append(1)
 
-  for df in neg_dfs:
-    arr = df.to_numpy()
+  for filename in neg_class:
+    file_path = os.path.join(args.neg_folder, filename)
+    data = pd.read_csv(file_path, sep=',', header = None)
+    data.drop(index=data.index[0], 
+              axis=0, 
+              inplace=True)
+    data = data.apply(pd.to_numeric)
+    arr = data.to_numpy()
+    np.reshape(arr,(1, 18001, 15))
     X.append(arr)
     Y.append(0)
+
+  X = np.array(X)
+  Y = np.array(Y)
+
+  tam = len(Y)
+  np.reshape(Y,(1, 1, tam))
 
   return X, Y
 
 
 def split(X, Y):
 
-  tam = len(Y)
-
   X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 0)
-
-  X_train = np.reshape(X_train,(1, 18001, 15))
-  X_test = np.reshape(X_test,(1, 18001, 15))
-  y_train = np.reshape(y_train,(1, tam, 15))
-  y_test = np.reshape(y_test,(1, tam, 15))
-
 
   model = keras.Sequential(
     [
@@ -74,29 +69,45 @@ def split(X, Y):
   return model, X_train, X_test, y_train, y_test
 
 
-
-
 def train_model(args):
 
-  pos_dfs, neg_dfs = input_fn(args)
-  X, Y = preprocess(pos_dfs=pos_dfs, neg_dfs=neg_dfs)
+  X, Y = input_fn(args)
   
   model, X_train, X_test, y_train, y_test = split(X, Y)
 
-  model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=1, loss='binary_crossentropy')
+  model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-  model.compile(optimizer="rmsprop", loss='mse', metrics=['accuracy'])
+  callback_list = []
 
-  results = model.evaluate(X_test, y_test, batch_size=128)
+  model_name = 'keras_model.h5'
+  model_name_local = os.path.join('/tmp', model_name)
+  callback_checkpoint_path = os.path.join(args.job_dir, 'model', model_name)
+  callback_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            filepath=model_name_local,
+            save_best_only=False,
+            save_weights_only=False,  # https://github.com/tensorflow/tensorflow/issues/38745
+            save_freq='epoch'
+          )
+
+  callback_list.append(callback_checkpoint)
+
+  callback_tboard = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(args.job_dir, 'tensorboard'))
+
+  callback_list.append(callback_tboard)
+
+  model.fit(X_train, y_train, epochs=40, batch_size=32, verbose=1, validation_data=(X_test, y_test), callbacks=callback_list)
+
+  results = model.evaluate(X_test, y_test, batch_size=1)
 
   return results
 
 
 if __name__ == '__main__':
 
-  parser = argparse.ArgumentParser(description='Conform data to a new csv.')
+  parser = argparse.ArgumentParser(description='Train the model.')
   parser.add_argument('--pos_folder', type=PathType(exists=True, type='dir'), required=True, help='The path for your folder containing all positive class.')
   parser.add_argument('--neg_folder', type=PathType(exists=True, type='dir'), required=True, help='The path for your folder containing all negative class.')
+  parser.add_argument('--job_dir', type=PathType(exists=True, type='dir'), required=True, help='The path for saving the model.')
 
   args = parser.parse_args()
 
